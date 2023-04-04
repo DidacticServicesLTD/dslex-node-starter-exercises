e6553446efc08b2186aa1446cfa2e74658e7c8f8
Calum Knott <calum@calumk.com>
2023-04-04 08:32:24 +0100

    rewrite

--- exercise9.js
+++ exercise9.js
@@ -2,19 +2,20 @@ import { OPCUAClient} from "node-opcua";
 // this wont work, unless you run : 
 // npm install node-opcua 
 
+let opcuaEndpoint = "opc.tcp://Calums-MacBook-Pro.local:53530/OPCUA/SimulationServer"
+let nodeId = "ns=3;i=1003"
+
 
 let simpleRead = async () => {
 
     const client = OPCUAClient.create();
 
     console.log('Connecting');
-    await client.connect("opc.tcp://Calums-MacBook-Pro.local:53530/OPCUA/SimulationServer");
+    await client.connect(opcuaEndpoint);
 
     console.log('Creating Session');
     const session = await client.createSession();
     
-
-    const nodeId = "ns=3;i=1003";
     const dataValue = await session.read({ nodeId });
 
     console.log(` value = ${dataValue.value.value.toString()}`);
